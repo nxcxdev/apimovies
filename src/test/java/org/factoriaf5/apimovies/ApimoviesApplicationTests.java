32339acda9785732f7f5482f05d66ef1d2ec1b3b
Nicolás Albarenque Malnero <nxcxdev@gmail.com>
2023-12-18 11:46:30 +0100

    Init repo.

---
+++ src/test/java/org/factoriaf5/apimovies/ApimoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.apimovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
